fcb867d8e664194933b03b750ed0a2096a113743
Stephen Quan <squan@esri.com>
2023-10-31 08:38:05 +1100

    https://stackoverflow.com/questions/77376714/collectionview-binding-does-not-work-correctly

---
+++ StackOverflow.Maui.Mvvm.CultureInfoFlag/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StackOverflow.Maui.Mvvm.CultureInfoFlag
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ StackOverflow.Maui.Mvvm.CultureInfoFlag/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StackOverflow.Maui.Mvvm.CultureInfoFlag
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
