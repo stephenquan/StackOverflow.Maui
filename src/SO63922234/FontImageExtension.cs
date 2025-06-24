ba47ff9657237f363c04b2d61ee908c7dbf7ae45
Stephen Quan <squan@esri.com>
2025-06-24 19:03:56 +1000

    Use BindableObject as the base class

--- src/SO63922234/FontImageExtension.cs
+++ src/SO63922234/FontImageExtension.cs
@@ -6,7 +6,7 @@ namespace SO63922234;
 
 [ContentProperty(nameof(IconName))]
 [RequireService([typeof(IReferenceProvider), typeof(IProvideValueTarget)])]
-public partial class FontImageExtension : Element, IMarkupExtension<BindingBase>
+public partial class FontImageExtension : BindableObject, IMarkupExtension<BindingBase>
 {
 	[BindableProperty, NotifyPropertyChangedFor(nameof(Result))] public partial double Size { get; set; } = 30.0;
 	[BindableProperty, NotifyPropertyChangedFor(nameof(Result))] public partial string IconName { get; set; } = string.Empty;
@@ -23,9 +23,9 @@ public partial class FontImageExtension : Element, IMarkupExtension<BindingBase>
 	public object ProvideValue(IServiceProvider serviceProvider) => (this as IMarkupExtension<BindingBase>).ProvideValue(serviceProvider);
 	BindingBase IMarkupExtension<BindingBase>.ProvideValue(IServiceProvider serviceProvider)
 	{
-		if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget provideValueTarget && provideValueTarget.TargetObject is Element targetElement)
+		if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget provideValueTarget && provideValueTarget.TargetObject is BindableObject targetObject)
 		{
-			this.SetBinding(Element.BindingContextProperty, static (Element t) => t.BindingContext, BindingMode.OneWay, source: targetElement);
+			this.SetBinding(BindableObject.BindingContextProperty, static (BindableObject t) => t.BindingContext, BindingMode.OneWay, source: targetObject);
 		}
 		return BindingBase.Create(static (FontImageExtension t) => t.Result, BindingMode.OneWay, source: this);
 	}
--- src/SO74319669/RgbColorExtension.cs
+++ src/SO74319669/RgbColorExtension.cs
@@ -5,28 +5,28 @@ namespace SO74319669;
 /// <summary>Provides a markup extension for creating a color binding based on RGB values.</summary>
 [ContentProperty(nameof(R))]
 [RequireService([typeof(IReferenceProvider), typeof(IProvideValueTarget)])]
-public partial class RgbColorExtension : Element, IMarkupExtension<BindingBase>
+public partial class RgbColorExtension : BindableObject, IMarkupExtension<BindingBase>
 {
 	/// <summary>Gets or sets the red component of the color.</summary>
-	[BindableProperty, NotifyPropertyChangedFor(nameof(Color))] public partial float R { get; set; } = 0.0f;
+	[BindableProperty, NotifyPropertyChangedFor(nameof(Result))] public partial float R { get; set; } = 0.0f;
 	/// <summary>Gets or sets the green component of the color.</summary>
-	[BindableProperty, NotifyPropertyChangedFor(nameof(Color))] public partial float G { get; set; } = 0.0f;
+	[BindableProperty, NotifyPropertyChangedFor(nameof(Result))] public partial float G { get; set; } = 0.0f;
 	/// <summary>Gets or sets the blue component of the color.</summary>
-	[BindableProperty, NotifyPropertyChangedFor(nameof(Color))] public partial float B { get; set; } = 0.0f;
+	[BindableProperty, NotifyPropertyChangedFor(nameof(Result))] public partial float B { get; set; } = 0.0f;
 	/// <summary>Gets or sets the alpha component of the color.</summary>
-	[BindableProperty, NotifyPropertyChangedFor(nameof(Color))] public partial float A { get; set; } = 0.0f;
+	[BindableProperty, NotifyPropertyChangedFor(nameof(Result))] public partial float A { get; set; } = 0.0f;
 	/// <summary>Gets the color represented by the current RGBA values.</summary>
-	public Color Color => Color.FromRgba(R, G, B, A);
+	public Color Result => Color.FromRgba(R, G, B, A);
 	/// <summary>Returns a binding that provides the color based on the current RGBA values.</summary>
 	/// <param name="serviceProvider"></param>
 	/// <returns></returns>
 	public object ProvideValue(IServiceProvider serviceProvider) => (this as IMarkupExtension<BindingBase>).ProvideValue(serviceProvider);
 	BindingBase IMarkupExtension<BindingBase>.ProvideValue(IServiceProvider serviceProvider)
 	{
-		if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget provideValueTarget && provideValueTarget.TargetObject is Element targetElement)
+		if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget provideValueTarget && provideValueTarget.TargetObject is BindableObject targetObject)
 		{
-			this.SetBinding(Element.BindingContextProperty, static (Element t) => t.BindingContext, BindingMode.OneWay, source: targetElement);
+			this.SetBinding(BindableObject.BindingContextProperty, static (BindableObject t) => t.BindingContext, BindingMode.OneWay, source: targetObject);
 		}
-		return BindingBase.Create(static (RgbColorExtension t) => t.Color, BindingMode.OneWay, source: this);
+		return BindingBase.Create(static (RgbColorExtension t) => t.Result, BindingMode.OneWay, source: this);
 	}
 }
