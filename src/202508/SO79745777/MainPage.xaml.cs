1d440db816d72f1487b3be1e97e33a99acba367c
Stephen Quan <squan@esri.com>
2025-08-28 20:33:21 +1000

    Update XML doc

--- src/202508/SO79745777/MainPage.xaml.cs
+++ src/202508/SO79745777/MainPage.xaml.cs
@@ -1,13 +1,16 @@
 ﻿namespace SO79745777;
 
 /// <summary>
-/// 
+/// The application's main page.
 /// </summary>
 public partial class MainPage : ContentPage
 {
 	/// <summary>
-	/// 
+	/// Initializes a new instance of the <see cref="MainPage"/> class with the specified view model.
 	/// </summary>
+	/// <remarks>
+	/// This demonstrates how to connect the MessageView to the MainViewModel and handle the SendMessageEvent to display an alert.
+	/// </remarks>
 	public MainPage(MainViewModel vm)
 	{
 		BindingContext = vm;
@@ -15,4 +18,3 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 }
-
