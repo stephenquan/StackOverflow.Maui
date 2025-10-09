864412a11a6be271d589e7118bfcd63143a65597
Stephen Quan <squan@esri.com>
2025-10-09 13:49:32 +1100

    https://stackoverflow.com/questions/79696605/cant-pass-bound-view-model-as-command-parameter

---
+++ src/StackOverflow.Maui.App/2025/10/SO79696605/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Stephen Quan.
+// Licensed under the MIT license.
+
+using CommunityToolkit.Mvvm.Input;
+
+namespace StackOverflow.Maui.App.SO79696605;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class MainPage : ContentPage
+{
+	/// <summary>
+	/// Gets a list of photos to display in the collection view.
+	/// </summary>
+	public List<PhotoItemViewModel> Photos { get; } =
+	[
+		new PhotoItemViewModel { PhotoName = "Photo 1" },
+		new PhotoItemViewModel { PhotoName = "Photo 2" },
+		new PhotoItemViewModel { PhotoName = "Photo 3" },
+		new PhotoItemViewModel { PhotoName = "Photo 4" },
+		new PhotoItemViewModel { PhotoName = "Photo 5" },
+		new PhotoItemViewModel { PhotoName = "Photo 6" },
+		new PhotoItemViewModel { PhotoName = "Photo 7" },
+		new PhotoItemViewModel { PhotoName = "Photo 8" },
+		new PhotoItemViewModel { PhotoName = "Photo 9" },
+		new PhotoItemViewModel { PhotoName = "Photo 10" },
+	];
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MainPage"/> class.
+	/// </summary>
+	public MainPage()
+	{
+		BindingContext = this;
+		InitializeComponent();
+	}
+
+	[RelayCommand]
+	async Task ImageLongPressed(PhotoItemViewModel photo)
+	{
+		await DisplayAlert("Long Pressed", photo.PhotoName, "OK");
+	}
+}
