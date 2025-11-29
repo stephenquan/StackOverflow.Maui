5cf0493ad17821b03bffac87e4145ab5f4aaa915
Stephen Quan <squan@esri.com>
2025-11-29 11:19:32 +1100

    https://stackoverflow.com/questions/77928546/how-to-make-a-non-modal-sheet-using-bottomsheetdialog-in-net-maui

---
+++ src/StackOverflow.Maui.App/2024/02/SO77928546/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+namespace StackOverflow.Maui.App.SO77928546;
+
+/// <summary>
+/// Sample demonstrating a non-modal bottom sheet.
+/// </summary>
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MainPage"/> class.
+	/// </summary>
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	async void OnCounterClicked(object? sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+		{
+			CounterBtn.Text = $"Clicked {count} time";
+		}
+		else
+		{
+			CounterBtn.Text = $"Clicked {count} times";
+		}
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
---
+++ src/StackOverflow.Maui.App/2024/02/SO77928546/README.md
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/77928546/how-to-make-a-non-modal-sheet-using-bottomsheetdialog-in-net-maui
