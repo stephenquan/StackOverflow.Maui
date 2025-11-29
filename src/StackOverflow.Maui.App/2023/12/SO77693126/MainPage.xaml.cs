008c52e056bb5f37fce836595b85a7c3244be382
Stephen Quan <squan@esri.com>
2025-11-29 11:53:14 +1100

    https://stackoverflow.com/questions/77693126/how-to-create-a-toggle-button-group

---
+++ src/StackOverflow.Maui.App/2023/12/SO77693126/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Stephen Quan.
+// Licensed under the MIT license.
+
+namespace StackOverflow.Maui.App.SO77693126;
+
+/// <summary>
+/// Simulates a Segmented Control with a RadioButton group.
+/// </summary>
+public partial class MainPage : ContentPage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MainPage"/> class.
+	/// </summary>
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/StackOverflow.Maui.App/2023/12/SO77693126/README.txt
@@ -0,0 +1,1 @@
+https://stackoverflow.com/questions/77693126/how-to-create-a-toggle-button-group
